fb09d0c0a6af14989c13659bde5f45978dda79f3
ammanuejhb024 <you@example.com>
2025-05-07 10:29:27 +0200

    variavbles

--- Main.java
+++ Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(system.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
 
     }
