dc21f14e75c2a1dd36ef0474b8eb970a578fd01f
ammanuejhb024 <you@example.com>
2026-02-16 00:13:31 +0200

    starter

---
+++ src/main/java/RockPaperScissors/Game.java
@@ -0,0 +1,11 @@
+package RockPaperScissors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Game {
+    String[] outcomes = {"rock" ,"paper" , "scissor"};
+    String bot = Random.outcomes;
+
+}
