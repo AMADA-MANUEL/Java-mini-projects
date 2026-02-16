e68d16be0f1f3c21da0e0f7683218bb2d46e0777
ammanuejhb024 <you@example.com>
2026-02-16 07:15:50 +0200

    starter

--- src/main/java/RockPaperScissors/Game.java
+++ src/main/java/RockPaperScissors/Game.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Game {
-    String[] outcomes = {"rock" ,"paper" , "scissor"};
-    String bot = Random.outcomes;
+//    String[] outcomes = {"rock" ,"paper" , "scissor"};
+//    String bot = Random.outcomes;
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,6 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +8,41 @@ public class Main {
 
         List<String> outcomes = Arrays.asList("rock" ,"paper" , "scissor");
         Random random = new Random();
+        int index = random.nextInt(outcomes.size());
+        String randomoutcome = outcomes.get(index);
+        //System.out.println(randomoutcome);
+
+         Scanner playerobj = new Scanner(System.in);
+        System.out.println("Rock ,Paper , Scissors ?");
+
+        String playerans = playerobj.nextLine().toLowerCase();
+
+        String again = "again";
+
+
+
+        if (randomoutcome.equals(playerans)  ){
+            System.out.println("draw");
+        } else if (randomoutcome.equals("rock") && playerans.equals("paper") ){
+            System.out.println("player wins");
+        }
+        else if (randomoutcome.equals("paper") && playerans.equals("rock")){
+            System.out.println("bot wins");
+        } else if (randomoutcome.equals("paper") && playerans.equals("scissor")) {
+            System.out.println("player wins");
+        } else if (randomoutcome.equals("scissor") && playerans.equals("paper")){
+            System.out.println("bot wins");
+        } else if (randomoutcome.equals("rock") && playerans.equals("scissor")) {
+            System.out.println("player wins");
+        }
+        else if (randomoutcome.equals("scissor") && playerans.equals("rock")) {
+            System.out.println("player wins");}
+
+
+
+
+
+
 
     }
 }
