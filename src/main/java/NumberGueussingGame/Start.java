e430b08318f1f5da125ffd5f8330cf7cf8a80d8a
ammanuejhb024 <you@example.com>
2026-02-20 06:30:20 +0200

    wrong

---
+++ src/main/java/NumberGueussingGame/Start.java
@@ -0,0 +1,16 @@
+package NumberGueussingGame;
+
+public class Start {
+    public static void main(String[] args ){
+
+        for(int  i = 1 ; i <= 100 ; i ++ ){
+            int random =  Math.random(i);
+//wrong
+
+        }
+
+    }
+
+
+
+}
