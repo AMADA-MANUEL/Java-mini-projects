bd459d20501f7243dc8d580296966c9f1cefde2d
ammanuejhb024 <you@example.com>
2026-02-20 07:15:56 +0200

    game working

--- src/main/java/NumberGueussingGame/Start.java
+++ src/main/java/NumberGueussingGame/Start.java
@@ -1,11 +1,28 @@
 package NumberGueussingGame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args ){
 
-        for(int  i = 1 ; i <= 100 ; i ++ ){
-            int random =  Math.random(i);
+        for(int  i = 1 ; i <= 10; i ++ ){
+            int random = (int)( Math.random()*100) + 1;
+            //System.out.println(random);
+            Scanner playerobj = new Scanner(System.in);
+
+            System.out.println("Pick a number ");
+            System.out.println("You have 10 tries to play this game");
+            int playerans = Integer.parseInt(playerobj.nextLine());
+            if(playerans > random){
+                System.out.println("Number too big");
+            } else if (playerans < random) {
+                System.out.println("Number too small");
+
+            }
 //wrong
+
+
 
         }
 
